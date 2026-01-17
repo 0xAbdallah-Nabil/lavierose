de0615b164c4fca961017cfa0589359e2b35a7b8
abdallah nabil <abdallahnabil2003@gmail.com>
2026-01-17 23:22:55 +0200

    fix: update vite config for Verce

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/lavierose/",
+  base: "/", // Changed from "/lavierose/"
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 });
