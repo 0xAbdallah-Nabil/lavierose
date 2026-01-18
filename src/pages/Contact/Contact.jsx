7ffbf11bc587bea2f0b21d8f7c304994c46dda93
abdallah nabil <abdallahnabil2003@gmail.com>
2026-01-18 03:52:38 +0200

    update the contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -13,13 +13,12 @@ function Contact() {
             alt="La Vie Rose Logo"
           />
           <div className="absolute inset-0 bg-black opacity-50"></div>
-        </div >
-        <h1 className=" text-center w-full px-4 text-base sm:text-2xl md:text-3xl font-bold text-white z-10">
-          Ready to turn your vision into reality?
-        </h1>
-        <p>
-          Let’s connect and start planning an unforgettable experience together!
-        </p>
+        </div>
+        <div className="flex flex-col justify-between items-center z-10 ">
+          <h1 className=" text-center w-full px-4 text-base sm:text-2xl md:text-3xl font-bold text-white z-10">
+            Ready to turn your vision into reality?
+          </h1>
+        </div>
       </main>
       <ContactForm />
     </>
