eeda9596dbb44c0335396e51d918ffd615fbb1db
abdallah nabil <abdallahnabil2003@gmail.com>
2026-01-18 03:40:36 +0200

    edit base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,4 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // eslint-disable-next-line no-undef
-  base: process.env.VITE_BASE_PATH || "/lavierose",
 });
