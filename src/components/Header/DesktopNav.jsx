dd16ff3e3cacf169639bedc8393ae9fd9f610c11
abdallah nabil <abdallahnabil2003@gmail.com>
2026-01-23 05:20:37 +0200

    fix: Nav bar

--- src/components/Header/DesktopNav.jsx
+++ src/components/Header/DesktopNav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../assets/imgs/logo-enhanced.png";
+import { NavLink } from "react-router-dom";
 function DesktopNav() {
   // Note: Import these at the top of your actual file:
   // import { Link, NavLink } from "react-router-dom";
@@ -9,15 +10,15 @@ function DesktopNav() {
     <div className="hidden md:flex items-center justify-between w-full max-w-7xl mx-auto px-6">
       {/* Left Navigation Items */}
       <div className="flex gap-8 lg:gap-10 items-center flex-1 justify-end z-30">
-        <a href="/" className="text-lg lg:text-xl text-white transition-colors duration-300">
+        <NavLink to="/" className="text-lg lg:text-xl text-white transition-colors duration-300">
           Home
-        </a>
-        <a href="/services" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300">
+        </NavLink>
+        <NavLink to="/services" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300">
           Styling
-        </a>
-        <a href="/service-package" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300 whitespace-nowrap">
+        </NavLink>
+        <NavLink to="/service-package" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300 whitespace-nowrap">
           Service Package
-        </a>
+        </NavLink>
       </div>
 
       {/* Centered Logo */}
@@ -33,12 +34,12 @@ function DesktopNav() {
 
       {/* Right Navigation Items */}
       <div className="flex gap-8 lg:gap-10 items-center flex-1 justify-start z-30">
-        <a href="/our-story" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300 whitespace-nowrap">
+        <NavLink to="/our-story" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300 whitespace-nowrap">
           Our Story
-        </a>
-        <a href="/contact-us" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300">
+        </NavLink>
+        <NavLink to="/contact-us" className="text-lg lg:text-xl hover:text-rose-600 transition-colors duration-300">
           Contact
-        </a>
+        </NavLink>
       </div>
     </div>
   );
